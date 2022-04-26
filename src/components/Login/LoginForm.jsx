cad4c93db2db765f4506de2983d02704f554400a
John M <johnmaiconnp@hotmail.com>
2022-04-26 15:04:21 -0300

    add input and button components

---
+++ src/components/Login/LoginForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('https://dogs.origamid.dev/json/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => console.log('json', json));
+  };
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label='Usuário' type='text' name='username' />
+        <Input label='Senha' type='password' name='password' />
+
+        <Button type='submit'>Entrar</Button>
+      </form>
+      <Link to='/login/criar'>Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
